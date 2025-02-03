b269efece8292149df4e5c0cbb0accacc6725dd4
sebasRm <71810357+sebasRm@users.noreply.github.com>
2025-02-02 19:08:50 -0500

    Update conexionDB.ts

--- back-end/src/db/conexionDB.ts
+++ back-end/src/db/conexionDB.ts
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
 let sequelize: any;
-
-sequelize = new Sequelize("cocinApp", "root", "", {
+// LOCAL
+/*sequelize = new Sequelize("cocinApp", "root", "", {
   host: "localhost",
+  dialect: "mysql",
+});*/
+// NUBE
+sequelize = new Sequelize("cocinApp", "root", "MiZZLgOUlYNCsLaLeewavGAvnkMTqiSd", {
+  host: "mysql.railway.internal",
   dialect: "mysql",
 });
 
